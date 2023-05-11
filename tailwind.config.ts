6e2481c27f1ae6da23bc0b4b51735c67f53be552
navorite <navorite@gmail.com>
2023-05-11 18:22:39 +0300

    Restructure and update packages

---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from 'tailwindcss';
+
+export default {
+  content: ['./src/**/*.{html,js,ts,svelte}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
