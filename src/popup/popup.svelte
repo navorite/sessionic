8349d87e0a95030631cf9aacc81a3a4aa2c43129
navorite <navorite@gmail.com>
2023-05-11 12:41:02 +0300

    Create new component

--- src/popup/popup.svelte
+++ src/popup/popup.svelte
@@ -1,17 +1,9 @@
 <script lang="ts">
-  import { getExtensionRuntimeURL } from '@utils/extension';
-
-  function openOptionsPage() {
-    let optionsURL = getExtensionRuntimeURL('options');
-    window.open(optionsURL, 'svelte-web-extension-template');
-  }
+  import Button from '@components/Button.svelte';
 </script>
 
 <div class="mt-4 w-[200px] h-[100px] flex flex-col items-center">
   <h1>Popup</h1>
 
-  <button
-    class="mt-5 bg-sky-500 text-black py-2 px-4 rounded font-bold hover:bg-green-700 hover:text-white"
-    on:click="{openOptionsPage}">Open Options</button
-  >
+  <Button />
 </div>
