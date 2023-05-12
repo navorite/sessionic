83e6eeb12bcb4773ede2eefdc33fc65090d3b54c
navorite <navorite@gmail.com>
2023-05-12 19:10:51 +0300

    Add Icon types and improve types, adjust css

--- src/lib/types/navigation.ts
+++ src/lib/types/navigation.ts
@@ -1,1 +1,11 @@
 export type Page = 'popup' | 'options';
+export type Icon =
+  | 'default'
+  | 'options'
+  | 'save'
+  | 'delete'
+  | 'search'
+  | 'clear'
+  | 'open'
+  | 'redo'
+  | 'undo';
--- src/lib/utils/browser.ts
+++ src/lib/utils/browser.ts
@@ -1,21 +1,22 @@
 import browser from 'webextension-polyfill';
 
+type Tab = browser.Tabs.Tab;
 type QueryInfo = browser.Tabs.QueryQueryInfoType;
 
 // Get current active tab
 
-export async function getCurrentTab() {
+export async function getCurrentTab(): Promise<Tab[]> {
   return await getWindowTabs({ active: true });
 }
 
 // Get all tabs of current window
 
-export async function getWindowTabs(optionalQuery?: QueryInfo) {
+export async function getWindowTabs(optionalQuery?: QueryInfo): Promise<Tab[]> {
   return await getTabs({ ...optionalQuery, currentWindow: true });
 }
 
 // Get all tabs that match a query obj
 
-export async function getTabs(queryInfo?: QueryInfo) {
+export async function getTabs(queryInfo?: QueryInfo): Promise<Tab[]> {
   return await browser?.tabs?.query(queryInfo);
 }
