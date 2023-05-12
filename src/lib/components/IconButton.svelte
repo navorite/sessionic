0eb18c7680cd9219847dc9e6dc0491ae348822e4
navorite <navorite@gmail.com>
2023-05-12 19:09:53 +0300

    Add component for icons

---
+++ src/lib/components/IconButton.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { fade } from 'svelte/transition';
+  import type { Icon } from '../types/navigation';
+
+  export let icon: Icon = 'default';
+  export let title: string;
+
+  let className = '';
+  export { className as class };
+
+  $: src = `../../icons/${icon}.svg`;
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div
+  {title}
+  class="cursor-pointer {className}"
+  style:mask="url({src}) no-repeat center"
+  style:-webkit-mask="url({src}) no-repeat center"
+  style:mask-size="cover"
+  style:-webkit-mask-size="cover"
+  style:background-color="currentColor"
+  style:width="1em"
+  style:height="1em"
+  on:click
+/>
+<!-- transition:fade -->
