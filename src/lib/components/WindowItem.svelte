87aa2b71f1ca7ca847a1b9526aa7e19c2420796a
navorite <navorite@gmail.com>
2023-05-16 09:43:30 +0300

    More UI fixes

--- src/lib/components/WindowItem.svelte
+++ src/lib/components/WindowItem.svelte
@@ -10,46 +10,44 @@
   // let tabsURL = [];
 </script>
 
-<div class="w-full h-full">
-  <div
-    class="flex items-center gap-2 cursor-pointer group/w rounded-md bg-neutral-4 hover:bg-primary-5 py-3 px-2 mb-1"
+<div
+  class="flex items-center gap-2 cursor-pointer group/w rounded-md bg-neutral-4 hover:bg-primary-5 py-3 px-2 mb-1"
+>
+  <IconButton
+    icon={showWindow ? 'windowon' : 'windowoff'}
+    title="Collapse Window"
+    class="text-2xl hover:text-primary-pure-1"
+    on:click={() => {
+      showWindow = !showWindow;
+    }}
+  />
+
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <span
+    title="Open this Window in a New Window"
+    class="w-[90%] overflow-hidden whitespace-nowrap text-ellipsis hover:underline"
+    on:click={() => {
+      //   if (tabsURL.length) openWindow(tabsURL);
+      //   console.log(tabsURL);
+    }}
   >
-    <IconButton
-      icon={showWindow ? 'windowon' : 'windowoff'}
-      title="Window"
-      class="text-2xl"
-      on:click={() => {
-        showWindow = !showWindow;
-      }}
-    />
+    Window
+  </span>
 
-    <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <span
-      title="Open this Window in a New Window"
-      class="w-[90%] overflow-hidden whitespace-nowrap text-ellipsis hover:underline"
-      on:click={() => {
-        //   if (tabsURL.length) openWindow(tabsURL);
-        //   console.log(tabsURL);
-      }}
-    >
-      Window
-    </span>
-
-    <IconButton
-      icon="delete"
-      title="Remove this window from session"
-      class="invisible ml-auto text-2xl hover:text-warning-pure-1 group-hover/w:visible"
-    />
-  </div>
-
-  {#if showWindow}
-    <ul class="h-full pl-2">
-      {#each windowInfo?.tabs as tab}
-        <!-- {@const tabItem = tabsURL.push(tab.url)} -->
-        <li>
-          <TabItem tabInfo={tab} />
-        </li>
-      {/each}
-    </ul>
-  {/if}
+  <IconButton
+    icon="delete"
+    title="Remove this window from session"
+    class="invisible ml-auto text-2xl hover:text-warning-pure-1 group-hover/w:visible"
+  />
 </div>
+
+{#if showWindow}
+  <ul class="pl-2">
+    {#each windowInfo?.tabs as tab}
+      <!-- {@const tabItem = tabsURL.push(tab.url)} -->
+      <li>
+        <TabItem tabInfo={tab} />
+      </li>
+    {/each}
+  </ul>
+{/if}
