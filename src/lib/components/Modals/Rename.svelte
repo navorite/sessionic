b4fc734e20ed3a2322be65919192dcc2d36baa7f
navorite <navorite@gmail.com>
2023-06-09 13:55:33 +0300

    Add modal

---
+++ src/lib/components/Modals/Rename.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Modal from '@components/basic/Modal.svelte';
+
+  export let open;
+</script>
+
+<Modal bind:open>
+  <svelte:fragment slot="header">Rename session</svelte:fragment>
+  <button
+    type="button"
+    slot="actions"
+    class="px-4 py-1 bg-primary-4 rounded-md font-bold hover:bg-primary-5"
+    >Rename</button
+  >
+</Modal>
