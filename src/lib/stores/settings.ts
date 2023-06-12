a1dd4aac87e3e89aed282210641f49cf889601c4
navorite <navorite@gmail.com>
2023-06-12 16:38:02 +0300

    Cleanup code

--- src/lib/stores/settings.ts
+++ src/lib/stores/settings.ts
@@ -1,5 +1,4 @@
 import { writable, type Writable } from 'svelte/store';
 import type { FilterOptions } from '../types/extension';
 
-export const isTabPopup = writable(false);
 export const filterOptions: Writable<FilterOptions> = writable({ query: '' });
