670ca21eb23b9a1202f3de011c41cae01b8020ee
navorite <navorite@gmail.com>
2023-06-13 14:06:52 +0300

    Rework icon optimization and saved session size

---
+++ src/lib/constants/constants.ts
@@ -0,0 +1,12 @@
+export const tabAttr = [
+  'active',
+  'discarded',
+  'title',
+  'url',
+  'id',
+  'pinned',
+  'incognito',
+  'favIconUrl',
+];
+export const tabAttr_chrome = ['mutedInfo'];
+export const tabAttr_firefox = ['muted', 'isInReaderMode', 'cookieStoreId'];
--- src/lib/types/browser.ts
+++ src/lib/types/browser.ts
@@ -3,3 +3,8 @@ import browser from 'webextension-polyfill';
 export type Window = browser.Windows.Window;
 export type Tab = browser.Tabs.Tab;
 export type QueryInfo = browser.Tabs.QueryQueryInfoType;
+export type compressOptions = {
+  type?: 'image/webp' | 'image/jpeg' | 'image/png';
+  quality?: number;
+  max_size?: number;
+};
