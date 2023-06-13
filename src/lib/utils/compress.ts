1e285fb190e9aaa7262e5533acce96c83add52e7
navorite <navorite@gmail.com>
2023-06-13 17:44:40 +0300

    perf: enhance compression

--- src/lib/utils/compress.ts
+++ src/lib/utils/compress.ts
@@ -1,6 +1,5 @@
 import { isFirefox } from '@constants/env';
 import type { compressOptions } from '../types/browser';
-import type { Session } from '../types/extension';
 
 export default (() => {
   if (!isFirefox) return;
@@ -9,67 +8,31 @@ export default (() => {
   const ctx = canvas.getContext('2d');
   const img = document.createElement('img');
 
-  function optimizeFavIcons(url: string) {
-    if (!url) return;
-
-    return `https://www.google.com/s2/favicons?domain=${url}`;
-  }
-
-  async function icon(src: string, options?: compressOptions) {
-    if (!ctx || !img || !src) return;
-
-    return new Promise((resolve: (dataURL: string) => any) => {
-      img.src = src;
-      img.onload = (event) => {
-        if (event.currentTarget instanceof HTMLImageElement) {
-          if (
-            !options.max_size ||
-            options.max_size >= event.currentTarget.naturalWidth
-          ) {
-            options.max_size = event.currentTarget.naturalWidth;
-            options.quality = 1;
-          }
-
-          ctx.canvas.height = options.max_size;
-          ctx.canvas.width = options.max_size;
-
-          ctx.drawImage(
-            event.currentTarget,
-            0,
-            0,
-            options.max_size,
-            options.max_size
-          );
-
-          const data = ctx.canvas.toDataURL(options.type, options.quality);
-          //console.log({ before: img.src.length, after: data.length });
-          resolve(data);
-        }
-      };
-    });
-  }
-
-  async function session(
-    session: Session,
-    options?: compressOptions
-  ): Promise<unknown> {
-    return new Promise(async (resolve) => {
-      if (session?.windows?.length) {
-        for (const window of session.windows) {
-          for (const tab of window.tabs) {
-            tab.favIconUrl = await icon(tab.favIconUrl, options);
-          }
-        }
-
-        resolve(session);
-      }
-    });
-  }
-
   return {
-    optimizeFavIcons,
-    icon,
-    session,
+    async icon(src: string, options?: compressOptions) {
+      if (!ctx || !img || !src) return;
+
+      return new Promise((resolve: (dataURL: string) => any) => {
+        img.src = src;
+        img.onload = (event) => {
+          if (event.currentTarget instanceof HTMLImageElement) {
+            let { max_size, quality } = options;
+
+            if (!max_size || max_size >= event.currentTarget.naturalWidth) {
+              max_size = event.currentTarget.naturalWidth;
+              quality = 1;
+            }
+
+            ctx.canvas.height = max_size;
+            ctx.canvas.width = max_size;
+
+            ctx.drawImage(event.currentTarget, 0, 0, max_size, max_size);
+
+            resolve(ctx.canvas.toDataURL(options.type, quality));
+          }
+        };
+      });
+    },
   };
 })();
 
@@ -87,3 +50,10 @@ export default (() => {
 //   binaryArr[i] = decodedBinary.charCodeAt(i);
 
 // return URL.createObjectURL(new Blob([binaryArr], { type }));
+
+/*function optimizeFavIcons(url: string) {
+    if (!url) return;
+
+    return `https://www.google.com/s2/favicons?domain=${url}`;
+  }
+*/
