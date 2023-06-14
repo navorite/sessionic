054407725c0cd64fa12984718a49f137709293b8
navorite <navorite@gmail.com>
2023-06-14 17:57:18 +0300

    perf: do not render windows view when not in focus

--- src/lib/components/Windows/Windows.svelte
+++ src/lib/components/Windows/Windows.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { isExtensionReady } from '@utils/extension';
   import type { Session } from '../../types/extension';
   import Window from './Window.svelte';
 
@@ -9,7 +10,7 @@
   export { className as class };
 </script>
 
-{#if session}
+{#if session && isExtensionReady()}
   <ul class={className}>
     {#each session.windows as window}
       <Window {window} on:delete />
@@ -17,6 +18,6 @@
   </ul>
 {:else}
   <h2 class="text-lg font-bold mb-1 text-center">
-    Start opening some tabs and windows to view them here!
+    Start opening some tabs to view them here!
   </h2>
 {/if}
