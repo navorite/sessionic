03c574a286ade65f89eff2f438d6fb2cad1dbc8f
navorite <navorite@gmail.com>
2023-06-15 09:46:32 +0300

    feat: add ground work for command palette

--- src/popup/popup.svelte
+++ src/popup/popup.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
   import Header from '@components/Header.svelte';
   import Sessions from '@components/Sessions/Sessions.svelte';
+  import CommandPalette from '@components/basic/CommandPalette.svelte';
+
+  export let open = false;
 </script>
+
+<svelte:window
+  on:keydown={(event) => {
+    if (event.key === 'k' && event.ctrlKey) {
+      event.preventDefault();
+      if (!event.repeat) open = !open;
+    }
+  }}
+/>
 
 <Header />
 <Sessions />
+<CommandPalette bind:open />
