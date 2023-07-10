7f59037dfe28a8c99418ccd0b2240a1d5c7c19aa
navorite <navorite@gmail.com>
2023-07-10 22:24:18 +0300

    feat: add tab component

---
+++ src/options/components/basic/Tab.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  export let path: string;
+  export let title: string;
+
+  export let group: string = window?.location?.href.split('#')[1] ?? 'general';
+
+  let selected = false;
+
+  function getLocation() {
+    if (window?.location?.href?.endsWith(`#${path}`)) {
+      group = path;
+    }
+  }
+
+  $: console.log(window?.location?.href?.endsWith(`#${path}`), path, group);
+
+  $: selected = group === path;
+</script>
+
+<svelte:window on:hashchange={getLocation} />
+
+<a
+  href="#{path}"
+  class="{selected
+    ? 'bg-primary-focus text-white'
+    : 'bg-neutral-3'} p-2 rounded-md {selected
+    ? 'hover:bg-primary-focus'
+    : 'hover:bg-neutral-4'} font-semibold">{title}</a
+>
