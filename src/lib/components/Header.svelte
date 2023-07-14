1739ef1e3cee873c351bdae0c00cc06200930137
navorite <navorite@gmail.com>
2023-07-14 19:26:53 +0300

    prepare for beta release

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -17,6 +17,8 @@
 <div class="flex items-center w-full h-full max-h-8 gap-2">
   <h1>Tabify</h1>
 
+  <div role="note" class="card bg-primary text-white">BETA</div>
+
   <div aria-label="Number of sessions" class="card bg-primary text-white">
     {$filtered?.length ?? 0} Session{$filtered?.length ?? 0 > 1 ? 's' : ''}
   </div>
