5fc4bd7835c044b4f1d66e9cdfdbc7f7b66865f9
navorite <navorite@gmail.com>
2023-07-20 02:58:10 +0300

    fixup: improve tab icons fix

--- src/lib/utils/compress.ts
+++ src/lib/utils/compress.ts
@@ -5,16 +5,16 @@ export default (() => {
   if (!isFirefox) return;
 
   const img = document.createElement('img');
-  let ctx = document.createElement('canvas').getContext('2d');
+  const ctx = document.createElement('canvas').getContext('2d');
 
   return {
     async icon(src: string, tabURL: string, options?: compressOptions) {
-      if (!ctx || !img || !src) return src;
+      if (!ctx || !img || !src || !src.startsWith('data:')) return src;
 
       return new Promise((resolve: (dataURL: string) => any) => {
         img.src = src;
         img.onload = (event) => {
-          if (event.currentTarget instanceof HTMLImageElement && ctx) {
+          if (event.currentTarget instanceof HTMLImageElement) {
             let { max_size, quality } = options;
 
             if (!max_size || max_size >= event.currentTarget.naturalWidth) {
@@ -26,16 +26,10 @@ export default (() => {
             ctx.canvas.width = max_size;
 
             ctx.drawImage(event.currentTarget, 0, 0, max_size, max_size);
-            try {
-              const dataURL = ctx.canvas.toDataURL(options.type, quality);
 
-              resolve(src.length > dataURL.length ? dataURL : src);
-            } catch (err) {
-              ctx = document.createElement('canvas').getContext('2d');
+            const dataURL = ctx.canvas.toDataURL(options.type, quality);
 
-              log.warn('[compress.icon] failed to compress favicon ', tabURL);
-              resolve(src);
-            }
+            resolve(src.length > dataURL.length ? dataURL : src);
           }
         };
       });
