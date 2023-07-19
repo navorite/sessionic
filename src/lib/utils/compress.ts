5a0c43dd4270c4ef0e5113eae4ac8b79e06d5122
navorite <navorite@gmail.com>
2023-07-20 02:32:23 +0300

    fix: Session fails to load with bad tab icons
    Current Session fails to load when it has a tab that has a generic icon
    
    These icons occur when for example that site is offline or not loading

--- src/lib/utils/compress.ts
+++ src/lib/utils/compress.ts
@@ -1,20 +1,20 @@
 import { isFirefox } from '@constants/env';
+import log from './log';
 
 export default (() => {
   if (!isFirefox) return;
 
-  const canvas = document.createElement('canvas');
-  const ctx = canvas.getContext('2d');
   const img = document.createElement('img');
+  let ctx = document.createElement('canvas').getContext('2d');
 
   return {
-    async icon(src: string, options?: compressOptions) {
-      if (!ctx || !img || !src) return;
+    async icon(src: string, tabURL: string, options?: compressOptions) {
+      if (!ctx || !img || !src) return src;
 
       return new Promise((resolve: (dataURL: string) => any) => {
         img.src = src;
         img.onload = (event) => {
-          if (event.currentTarget instanceof HTMLImageElement) {
+          if (event.currentTarget instanceof HTMLImageElement && ctx) {
             let { max_size, quality } = options;
 
             if (!max_size || max_size >= event.currentTarget.naturalWidth) {
@@ -26,12 +26,16 @@ export default (() => {
             ctx.canvas.width = max_size;
 
             ctx.drawImage(event.currentTarget, 0, 0, max_size, max_size);
+            try {
+              const dataURL = ctx.canvas.toDataURL(options.type, quality);
 
-            const dataURL = ctx.canvas.toDataURL(options.type, quality);
+              resolve(src.length > dataURL.length ? dataURL : src);
+            } catch (err) {
+              ctx = document.createElement('canvas').getContext('2d');
 
-            if (dataURL.length > src.length) resolve(src);
-
-            resolve(dataURL);
+              log.warn('[compress.icon] failed to compress favicon ', tabURL);
+              resolve(src);
+            }
           }
         };
       });
