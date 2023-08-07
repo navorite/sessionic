c2d977f81585b8175cd8551cca12b0f398f13023
navorite <navorite@gmail.com>
2023-08-07 09:16:43 +0300

    feat(UI): Improve Switch UI

--- src/options/components/basic/Switch.svelte
+++ src/options/components/basic/Switch.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <label
-  class="flex max-w-max gap-2 items-center p-2 rounded-md font-medium"
+  class="flex max-w-max gap-2 items-center rounded-md font-medium text-sm cursor-pointer hover:brightness-125"
   use:tooltip={{ title: description }}
 >
   <input type="checkbox" aria-hidden="true" bind:checked on:change />
@@ -16,36 +16,25 @@
     role="switch"
     aria-checked={checked}
     aria-label={title}
-    class="block relative bg-neutral-6 w-[3.25rem] h-7 rounded-full before:fade before:content-[''] before:absolute before:top-0.5 before:left-0.5 before:block before:w-6 before:h-6 before:rounded-full before:bg-error"
+    class="block relative bg-neutral-6 w-11 h-6 rounded-full before:fade before:content-[''] before:bg-white before:absolute before:block before:w-6 before:h-6 before:rounded-full before:-left-0.5"
   />
   {title}
 </label>
 
 <style>
-  label:hover {
-    background-color: hsl(var(--neutral-3));
-    cursor: pointer;
-  }
-
   input {
     display: none; /*not the best for accessibility*/
   }
 
-  label:hover span {
-    border-color: hsl(var(--neutral-content));
-  }
-
-  label:hover span:before {
-    background-color: hsl(var(--error-focus));
+  input:checked + span {
+    background-color: hsl(var(--primary));
   }
 
   input:checked + span:before {
     translate: 100% 0%;
-    background-color: hsl(var(--success));
   }
 
-  input:checked:hover + span:before {
-    translate: 100% 0%;
-    background-color: hsl(var(--success-focus));
+  label:hover > span::before {
+    background-color: white;
   }
 </style>
