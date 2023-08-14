e842c1df72fe606ed6ff2407da59e74927e8914b
navorite <navorite@gmail.com>
2023-08-14 16:38:38 +0300

    add extension info to about page

--- src/lib/components/options/pages/About.svelte
+++ src/lib/components/options/pages/About.svelte
@@ -3,10 +3,18 @@
 </script>
 
 <Section title="About">
+	<div class="flex flex-col items-baseline gap-2 text-sm font-medium">
+		<p class="text-sm">
+			{__EXT_NAME__}
+			{__EXT_VER__}
+		</p>
+		<a href="https://github.com/navorite/sessionic/releases">Changelog</a>
+		<a href="https://github.com/navorite/sessionic">Source code (AGPL v3)</a>
+	</div>
+
 	<p>
-		This extension was created by <a
-			href="https://github.com/navorite"
-			class="font-bold hover:text-primary-focus hover:underline">@navorite</a
+		This extension was created by <a href="https://github.com/navorite"
+			>@navorite</a
 		>
 	</p>
 
@@ -14,3 +22,9 @@
 
 	<Donate />
 </Section>
+
+<style lang="postcss">
+	a {
+		@apply font-medium text-link hover:text-link-focus;
+	}
+</style>
