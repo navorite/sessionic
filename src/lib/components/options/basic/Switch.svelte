5047cf74835da73d98024dcf6db502480d0f40e9
navorite <navorite@gmail.com>
2023-08-17 00:15:01 +0300

    enhance: Enhance Switch UI

--- src/lib/components/options/basic/Switch.svelte
+++ src/lib/components/options/basic/Switch.svelte
@@ -10,31 +10,29 @@
 	class="flex max-w-max cursor-pointer items-center gap-2 rounded-md text-sm font-medium hover:brightness-125"
 	use:tooltip={{ title: description }}
 >
-	<input type="checkbox" aria-hidden="true" bind:checked on:change />
+	<input
+		type="checkbox"
+		aria-hidden="true"
+		bind:checked
+		on:change
+		class="hidden"
+	/>
 	<span
 		tabindex="0"
 		role="switch"
 		aria-checked={checked}
 		aria-label={title}
-		class="before:fade relative block h-6 w-11 rounded-full bg-neutral-6 before:absolute before:-left-0.5 before:block before:h-6 before:w-6 before:rounded-full before:bg-white before:content-['']"
+		class="before:fade relative mr-1 block h-6 w-11 rounded-full bg-neutral-content/10 before:absolute before:left-0.5 before:top-0.5 before:block before:h-5 before:w-5 before:rounded-full before:bg-neutral-1 before:content-['']"
 	/>
 	{title}
 </label>
 
 <style>
-	input {
-		display: none; /*not the best for accessibility*/
-	}
-
 	input:checked + span {
 		background-color: hsl(var(--primary));
 	}
 
 	input:checked + span:before {
 		translate: 100% 0%;
-	}
-
-	label:hover > span::before {
-		background-color: white;
 	}
 </style>
