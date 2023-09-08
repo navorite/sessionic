e5aba2087545376050dd6275208011c1ff074141
navorite <navorite@gmail.com>
2023-09-08 13:15:29 +0300

    return default value for storage item

--- src/lib/utils/storage.ts
+++ src/lib/utils/storage.ts
@@ -1,8 +1,11 @@
 import type { ESettings } from '@/lib/types';
 import { storage } from 'webextension-polyfill';
 
-export async function getStorageItem<K extends keyof ESettings>(key: K) {
-	return (await storage.local.get(key))[key] as ESettings[K];
+export async function getStorageItem<K extends keyof ESettings>(
+	key: K,
+	defaultVal: ESettings[K]
+) {
+	return ((await storage.local.get(key))[key] as ESettings[K]) ?? defaultVal;
 }
 
 export async function getStorage<T extends ESettings>(
