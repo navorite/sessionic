3d8526b932e231a8131bf90f60eaa7a7f5d16701
navorite <navorite@gmail.com>
2023-09-14 17:25:05 +0300

    add colorinput component

---
+++ src/lib/components/options/basic/ColorInput.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	export let color: string;
+</script>
+
+<label class="flex max-w-max items-center gap-1">
+	<slot />
+	<span
+		class="h-8 w-16 cursor-pointer rounded-md border-2 border-gray-600"
+		style:background-color={color}
+	>
+		<input
+			type="color"
+			class="invisible h-full w-full"
+			bind:value={color}
+			on:change
+		/>
+	</span>
+</label>
