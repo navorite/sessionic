d9eb54971a5aa27e2a04330d4f6f258643f7f00e
navorite <navorite@gmail.com>
2023-09-14 17:25:28 +0300

    add default imports

--- src/lib/components/options/basic/index.ts
+++ src/lib/components/options/basic/index.ts
@@ -2,3 +2,5 @@ export { default as Section } from './Section.svelte';
 export { default as Shortcut } from './Shortcut.svelte';
 export { default as Switch } from './Switch.svelte';
 export { default as Tab } from './Tab.svelte';
+export { default as ColorInput } from './ColorInput.svelte';
+export { default as TagEditor } from './TagEditor.svelte';
