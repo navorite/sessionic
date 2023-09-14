d19d01eabb8671376b5f66400fad4cca1c933f5c
navorite <navorite@gmail.com>
2023-09-14 19:03:38 +0300

    add ability to create and inspect tag in tag modal

--- src/lib/components/Modals/TagsModal.svelte
+++ src/lib/components/Modals/TagsModal.svelte
@@ -1,11 +1,24 @@
 <script lang="ts">
 	import { createEventDispatcher } from 'svelte';
-	import { Modal } from '@/lib/components';
+	import { Modal, ColorInput, Tag } from '@/lib/components';
 	import { getStorageItem } from '@/lib/utils';
+	import { settings } from '@/lib/stores';
 
 	export let open = false;
 
 	let tags: Record<string, { bgColor: string; textColor: string }>;
+
+	let selectVal = 'createANewTag';
+
+	let tag = { name: 'Preview', bgColor: 'royalblue', textColor: 'white' };
+
+	$: selectVal === 'createANewTag'
+		? { name: 'Preview', bgColor: 'royalblue', textColor: 'white' }
+		: (tag = {
+				name: selectVal,
+				bgColor: $settings.tags[selectVal]?.bgColor ?? 'royalblue',
+				textColor: $settings.tags[selectVal]?.textColor ?? 'white'
+		  });
 
 	$: if (open)
 		getStorageItem('tags', {}).then((value) => {
@@ -15,24 +28,35 @@
 	const dispatch = createEventDispatcher<{ tagSubmit: string }>();
 </script>
 
-<Modal bind:open height="12rem">
-	<svelte:fragment slot="header">Select a tag</svelte:fragment>
+<Modal bind:open height="full">
+	<svelte:fragment slot="header">Assign a tag</svelte:fragment>
 	<svelte:fragment slot="content">
 		<form
-			class="flex h-full w-full flex-col justify-around"
-			on:submit|preventDefault={(event) => {
-				const result = new FormData(event.currentTarget);
+			class="flex h-full w-full flex-col justify-around gap-4"
+			on:submit|preventDefault={() => {
+				if (selectVal === 'createANewTag') {
+					const tags = $settings.tags;
 
-				const tag = result.getAll('tags')[0];
+					tags[tag.name] = {
+						bgColor: tag.bgColor,
+						textColor: tag.textColor
+					};
 
-				if (!tag || typeof tag !== 'string') return;
+					settings.changeSetting('tags', tags);
 
-				dispatch('tagSubmit', tag);
+					dispatch('tagSubmit', tag.name);
+				} else dispatch('tagSubmit', selectVal);
 
 				open = false;
 			}}
 		>
-			<select name="tags" id="tags" class="rounded-md p-1">
+			<select
+				name="tags"
+				id="tags"
+				class="rounded-md p-1"
+				bind:value={selectVal}
+			>
+				<option value={'createANewTag'}>Create new</option>
 				{#if tags}
 					{#each Object.keys(tags) as tag}
 						<option
@@ -43,6 +67,42 @@
 					{/each}
 				{/if}
 			</select>
+
+			{#if selectVal === 'createANewTag'}
+				<div class="flex flex-col gap-2">
+					<label>
+						Name:
+						<input
+							type="text"
+							minlength="1"
+							maxlength="12"
+							placeholder="e.g. Personal"
+							class="rounded-md px-2 py-1"
+							value={tag.name}
+							on:input={(event) => {
+								const value = event.currentTarget.value;
+
+								if (value.length < 1 || value.length > 12)
+									return (event.currentTarget.value = tag.name);
+
+								tag.name = value;
+							}}
+						/></label
+					>
+					<ColorInput bind:color={tag.bgColor}>Background:</ColorInput>
+					<ColorInput bind:color={tag.textColor}>Text:</ColorInput>
+				</div>
+			{/if}
+
+			<span class="flex gap-2">
+				Preview:
+				<Tag
+					bind:name={tag.name}
+					bind:bgColor={tag.bgColor}
+					bind:textColor={tag.textColor}
+				/>
+			</span>
+
 			<button class="rounded-md bg-primary px-2 py-1 hover:bg-primary-focus"
 				>Submit</button
 			>
