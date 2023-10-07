bf12623ca6d848805e57f7f0a5c61c4a2483bcdd
navorite <navorite@gmail.com>
2023-10-07 05:40:30 +0300

    enhance: Improve relative date format

--- src/lib/utils/getRelativeTime.ts
+++ src/lib/utils/getRelativeTime.ts
@@ -1,19 +1,20 @@
 const timeInMS = {
-	year: 31536000000,
-	month: 2628000000,
+	yr: 31536000000,
+	mo: 2628000000,
 	day: 86400000,
-	hour: 3600000,
-	minute: 60000
+	hr: 3600000,
+	min: 60000
 };
 
 export function getRelativeTime(date: number) {
 	const elapsed = Date.now() - date;
 
 	for (const unit in timeInMS) {
-		if (elapsed > timeInMS[unit as keyof typeof timeInMS])
-			return `${Math.round(elapsed / timeInMS[unit as keyof typeof timeInMS])}${
-				unit[0]
-			} ago`;
+		if (elapsed > timeInMS[unit as keyof typeof timeInMS]) {
+			const val = Math.round(elapsed / timeInMS[unit as keyof typeof timeInMS]);
+
+			return `${val} ${unit}`;
+		}
 	}
 
 	return 'just now';
