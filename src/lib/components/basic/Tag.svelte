b607a96d3470f650aa2256d56f90393248c84de4
navorite <navorite@gmail.com>
2023-10-09 19:12:12 +0300

    use full height instead on tag

--- src/lib/components/basic/Tag.svelte
+++ src/lib/components/basic/Tag.svelte
@@ -12,7 +12,7 @@
 <span
 	style:background-color={bgColor}
 	style:color={textColor}
-	class="my-auto inline-block w-full max-w-[8rem] cursor-default overflow-hidden overflow-ellipsis whitespace-nowrap rounded-md bg-primary px-1 py-0.5 text-center text-xs font-semibold text-white {className}"
+	class="inline-block h-full w-full max-w-[8rem] cursor-default overflow-hidden overflow-ellipsis whitespace-nowrap rounded-md bg-primary px-1 py-0.5 text-center text-xs font-semibold text-white {className}"
 	use:tooltip={{ title: name }}
 >
 	{name}
